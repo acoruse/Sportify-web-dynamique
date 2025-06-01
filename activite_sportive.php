bac4111fb3e2d2716b982d18c9babaeb248d054b
Emilesch12 <149698107+Emilesch12@users.noreply.github.com>
2025-06-01 10:59:04 +0200

    Update activite_sportive.php

--- activite_sportive.php
+++ activite_sportive.php
@@ -1,13 +1,1 @@
-<?php
-include('connection_BDD.php');// afin de se connecter a la BDD
-session_start();// afin de savoir si l'on est connecte ou non
-// on verifie si l utilisateur est co
-if (!isset($_SESSION['id'])) {
-    header('Location: compte.php');
-    exit();
-}
-$activites = ["Musculation", "Fitness", "Biking", "Cardio-Training", "Cours Collectifs"];// on liste les activites
-$coachs = [];//on cree le tableau vide coach 
 
-if (isset($_GET['activite'])) {
-    $activite = $_GET['activite'];
